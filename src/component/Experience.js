d690a74eb358c2f91ad69c1ffd4a70e1490f31dc
Alfaj Shaikh <alfaj.shaikh@tekditechnologies.com>
2024-02-07 11:51:43 +0530

    Initial Camera Position to view the object

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -7,17 +7,26 @@ export const Experience = () => {
     const { camera, gl } = useThree();
     const controls = useRef();
 
+    // Set the initial camera position
+    useEffect(() => {
+        camera.position.set(-1760.4805421048945, Math.max(337.31586052580525, 50), -1807.9851536299686);
+    }, [camera]);
+
+    // Adjust controls during updates
     useEffect(() => {
         const handleControlsUpdate = () => {
             // Ensure that the Y position of the camera doesn't go below 50
             camera.position.setY(Math.max(camera.position.y, 50));
         };
 
-        controls.current.addEventListener('change', handleControlsUpdate);
+        // Add a null check before attempting to remove the event listener
+        if (controls.current) {
+            controls.current.addEventListener('change', handleControlsUpdate);
 
-        return () => {
-            controls.current.removeEventListener('change', handleControlsUpdate);
-        };
+            return () => {
+                controls.current.removeEventListener('change', handleControlsUpdate);
+            };
+        }
     }, [camera]);
 
     return (
